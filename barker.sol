1176d8c58dd916edb84b57f68680370ff24dfff1
Yukai Doge <60722582+YukaiDoge@users.noreply.github.com>
2021-05-04 10:34:40 +0700

    Update barker.sol

--- barker.sol
+++ barker.sol
@@ -2,37 +2,37 @@ pragma solidity =0.8.0;
 
 interface IBarker {
     function author() external view returns (address);
-    function replyingTo() external view returns (address);
+    function rebarkTo() external view returns (address);
     function content() external view returns (string memory);
     function claimableTips() external view returns (uint256);
     function claimTips() external;
-    event Bark(address author, address replyingTo, string content, uint256 tips);
+    event Bark(address author, address rebarkingTo, string content, uint256 tips);
     event Claimed(address author, address claimer, uint256 amount);
     event TipReceived(address thisBark, address tipper, uint256 amount);
 }
 
 contract Barker is IBarker {
     address payable private _author;
-    address payable private _replyingTo;
+    address payable private _rebarkingTo;
     string private _content;
     
-    constructor(address payable replyingTo_, string memory content_) payable {
+    constructor(address payable rebarkingTo_, string memory content_) payable {
         _author = payable(msg.sender);
-        _replyingTo = replyingTo_;
+        _rebarkingTo = rebarkingTo_;
         _content = content_;
         
-        // Send tip when replying (optional).
-        _replyingTo.transfer(msg.value);
+        // Send tip when rebarking (optional).
+        _rebarkingTo.transfer(msg.value);
 
-        emit Bark(_author, _replyingTo, _content, msg.value);
+        emit Bark(_author, _rebarkingTo, _content, msg.value);
     }
 
     function author() public view override returns (address) {
         return _author;
     }
     
-    function replyingTo() public view override returns (address) {
-        return _replyingTo;
+    function rebarkTo() public view override returns (address) {
+        return _rebarkingTo;
     }
 
     function content() public view override returns (string memory) {
