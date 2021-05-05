3de469f0ace1dbf24bb16700ea01e47bd66c395b
Yukai Doge <60722582+YukaiDoge@users.noreply.github.com>
2021-05-05 11:11:59 +0700

    Update barker_short.sol

--- barker_short.sol
+++ barker_short.sol
@@ -1,33 +1,32 @@
-// Tried a shorter version to save gas(?)
 pragma solidity =0.8.0;
 
 interface IBarker {
-    function claimTips() external;
-    event Bark(address author, address rebarkTo, string content, uint256 tips);
-    event Claimed(address author, address claimer, uint256 amount);
+    function collectTips() external;
+    event Bark(address author, address barkingToWhom, string content, uint256 tip);
     event TipReceived(address thisBark, address tipper, uint256 amount);
+    event TipsCollected(address author, address collector, uint256 amount);
 }
 
 contract Barker is IBarker {
     address payable public author;
-    address payable public rebarkTo;
+    address payable public barkingToWhom;
     string public content;
     
-    constructor(address payable rebarkTo_, string memory content_) payable {
+    constructor(address payable barkingToWhom_, string memory content_) payable {
         author = payable(msg.sender);
-        rebarkTo = rebarkTo_;
+        barkingToWhom = barkingToWhom_;
         content = content_;
-        rebarkTo.transfer(msg.value);
-        emit Bark(author, rebarkTo, content, msg.value);
+        barkingToWhom.transfer(msg.value);
+        emit Bark(author, barkingToWhom, content, msg.value);
     }
-    
-    function claimTips() public override {
-        uint256 amount = address(this).balance;
-        author.transfer(amount);
-        emit Claimed(author, msg.sender, amount);
-    }
-    
+
     receive() external payable {
         emit TipReceived(address(this), msg.sender, msg.value);
+    }
+
+    function collectTips() public override {
+        uint256 amount = address(this).balance;
+        author.transfer(amount);
+        emit TipsCollected(author, msg.sender, amount);
     }
 }
